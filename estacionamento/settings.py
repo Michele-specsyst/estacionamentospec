b6148d08de407d0c465ec276b3f8dec065457807
Michele-specsyst <michele@specsyst.com.br>
2021-01-26 14:23:40 -0300

    Atualizacao settings-ALLOWED_HOSTS

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'https://estacionamentospec.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'estacionamentospec.herokuapp.com/',]
 
 
 # Application definition
